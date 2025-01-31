bd86b1b3082ac66c77c991955295efb17b60916d
Tr1ck5t3r <tarungovindharaj2003@gmail.com>
2025-01-31 23:03:20 +0530

    pastel themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import sitemap from '@astrojs/sitemap';
 import playformCompress from '@playform/compress';
 import compressor from 'astro-compressor';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://effective-spoon-gw4xr65xvqjfvjq4.github.dev/',
-    integrations: [mdx(), sitemap(), playformCompress(), compressor({ gzip: true, brotli: false })],
+    integrations: [mdx(), sitemap(), playformCompress(), compressor({ gzip: true, brotli: false }), solidJs()],
 });
